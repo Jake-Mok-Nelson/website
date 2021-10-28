76b95cea0d37f5fc5bccd93bf9877055a1854d7c
Jake Mok Nelson <jakesmnelson@gmail.com>
2021-10-29 01:19:54 +1100

    Makefile for deplpoyments

---
+++ Makefile
@@ -0,0 +1,13 @@
+.PHONY: init start build deploy
+
+init:
+	yarn --cwd ./jakenelson.cloud/ install
+
+start: init
+	yarn --cwd ./jakenelson.cloud/ start
+
+build: init
+	yarn --cwd ./jakenelson.cloud/ build
+
+deploy:
+	GIT_USER=Jake-Mok-Nelson yarn --cwd ./jakenelson.cloud/ deploy
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd jakenelson.cloud && \ 
-    yarn build && \
-    GIT_USER=Jake-Mok_nelson yarn deploy && \
-    echo "Deployment successful, make sure you push the changes to save them!" || echo "Deployment failed, maybe revert the last change and deploy again."
-cd ..
